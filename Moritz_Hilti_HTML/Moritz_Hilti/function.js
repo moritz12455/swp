e82159c756ddc9b392b326508aa6171732c9b54c
moritz12455 <moritz12455>
2023-12-11 11:06:40 +0100

    cleared repo

--- Moritz_Hilti_HTML/Moritz_Hilti/function.js
+++
@@ -1,9 +0,0 @@
-function frageName() {
-    var name = prompt("Bitte geben Sie Ihren Namen ein:");
-    if (name != null && name !== "") {
-      alert("Hallo, " + name + "! Willkommen auf meiner Website.");
-    } else {
-      alert("Ungültige Eingabe. Bitte geben Sie Ihren Namen ein.");
-    }
-  }
-  
