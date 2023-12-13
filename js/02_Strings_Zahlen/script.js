6b96331c21655987d3979aed3cfa57e249e169d7
moritz12455 <moritz12455>
2023-12-13 16:50:31 +0100

    Added Übung 02 Stings und Zahlen

---
+++ js/02_Strings_Zahlen/script.js
@@ -0,0 +1,15 @@
+let a = 17.4;
+let b = 3.9;
+
+let result = a / b;
+
+// +, -, *, /
+
+console.log("The result of " + a + " divided by " + b + " is " + result )
+
+let firstname = "Moritz "
+let lastname = "Hilti"
+ 
+result = firstname + lastname
+
+console.log("Ich heisse " + result)
