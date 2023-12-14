78c25ab94e0956affc1727fe37fe0e100bf48375
moritz12455 <moritz12455>
2023-12-14 09:15:40 +0100

    Added Aufgabe 03

---
+++ js/03/script.js
@@ -0,0 +1,26 @@
+
+
+let bananaPricePerKilo = 2.14;
+let applePricePerKilo = 3.43;
+
+let bananaweight = 0.22;
+let appleweight = 0.34;
+
+
+let apple = 8;
+let result = 8 * appleweight * applePricePerKilo;
+
+console.log("7 Äpfel kosten " + result +"$");
+
+let banana = 17;
+result = 8 * bananaweight * bananaPricePerKilo;
+
+console.log("17 bananen kosten " + result +"$");
+
+bananaweight = 1000;
+result = bananaweight * bananaPricePerKilo
+console.log("1 Tonne Bananen kosten " + result +"$");
+
+appleweight = 1000;
+result = appleweight * applePricePerKilo
+console.log("1 Tonne Äpfel kosten " + result +"$");
