0d23d6bdcccacadecd3a217800333653aea64fc5
moritz12455 <moritz12455>
2024-01-08 11:30:27 +0100

    Test 20240108

--- tests/Test 20240108/script.js
+++ tests/Test 20240108/script.js
@@ -1,1 +1,16 @@
-/
+let kleinerGleich12 = 0;
+let groesserAls12 = 0;
+
+for (let i = 0; i < 10; i++) {
+    let zufallszahl = Math.floor(Math.random() * (15 - 5 + 1) + 5);
+    
+    if (zufallszahl > 12) {
+        console.log(`Zufallszahl ${zufallszahl} ist größer als 12.`);
+        groesserAls12++;
+    } else {
+        kleinerGleich12++;
+    }
+}
+
+console.log(`\nAnzahl der Zahlen kleiner-gleich 12: ${kleinerGleich12}`);
+console.log(`Anzahl der Zahlen größer als 12: ${groesserAls12}`);
