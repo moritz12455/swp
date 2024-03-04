18a72c9385e8aafe999f1d383629b00c5203bf47
moritz12455 <moritz12455>
2024-03-04 11:36:43 +0100

    Added Aufgabe JS 04

---
+++ js/04_Schleifen/script.js
@@ -0,0 +1,30 @@
+console.log("hallo")
+
+
+for (let count = 0; count < 8; count++) {
+    console.log("Moritz")
+}
+// Zähle von 7 bis 45 
+
+// Zähle von 9 bis 99 
+
+// Zähle rückwärts von 10 bis 0 
+
+// Zähle rückwärts von 1234 bis 1207 
+
+
+for (let count = 7; count <= 45; count++) {
+    console.log(count)
+}
+
+for (let count = 9; count <= 99; count++) {
+    console.log(count)
+}
+
+for (let count = 10; count >= 0; count--) {
+    console.log(count)
+}
+
+for (let count = 1234; count >= 1207; count--) {
+    console.log(count)
+}
