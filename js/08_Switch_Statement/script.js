ef4361807f619a7e61a173a72cc20127e462e4a2
moritz12455 <moritz12455>
2024-03-05 08:42:15 +0100

    Added Aufgabe 08_Switch_Statment

---
+++ js/08_Switch_Statement/script.js
@@ -0,0 +1,23 @@
+
+let randomnumber = Math.floor(Math.random() * (10 - 5 + 1) + 5);
+
+switch (randomnumber) {
+    case 10:
+        console.log("Ten")
+        break;
+    case 9:
+        console.log("Nine")
+        break;
+    case 8:
+        console.log("Eight")
+        break;
+    case 7:
+        console.log("Seven")
+        break;
+    case 6:
+        console.log("Six")
+        break;
+    case 5:
+        console.log("Five")
+        break;
+}
