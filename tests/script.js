e60dd212693f261e838c74ebe328ed8df143eebc
moritz12455 <moritz12455>
2024-06-10 10:56:09 +0200

    ran

--- tests/script.js
+++ tests/script.js
@@ -19,7 +19,13 @@ let zahlen = zahlentext.split(",")
 let max = Math.max(...zahlen);	
 console.log(max );
 // wie oft es vorkommt kann ich noch nicht 
-
+let counter = 0;
+for (let i = 0; i < zahlen.length; i++) {
+    if (Math.max(...zahlen) == zahlen[i]) {
+        counter++;
+    }
+}
+console.log(counter);
 
 const temperatures = "-1,5,51,-10,-11,2,90,-8,-44";
 
@@ -27,11 +33,14 @@ let temps = temperatures.split(",")
 let sum = 0;
 let count = 0;
 for (let i = 0; i<temps.length; i++) {
-    if( parseInt(temps [i]) < 50 && parseInt(temps [i]) [i] > -30){
+    if( parseInt(temps [i]) < 50 && parseInt(temps [i]) > -30){
     sum += parseInt(temps[i]);
     count++
 }
 }
+
+console.log("SUMME" +sum);
+console.log(count);
 
 let avg = sum / count;
 console.log(avg);
